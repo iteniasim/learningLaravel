3c96ab6e6debebf68128b7c1ae4846bb974b537e
Asim Iteni <iteniasim.hulkstar@Gmail.com>
2019-03-06 20:09:57 +0545

    The Reply Form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -30,6 +30,25 @@
             <br>
             @endforeach
 
+            @if (auth()->check())
+            <div class="card p-2">
+                <form action="{{ $thread->path(). '/replies' }}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <lable for="body">Body:</lable>
+                        <textarea name="body" id="body" class="form-control" rows="5"></textarea>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <div class="form-group">
+                            <button type="Submit" class="btn btn-primary">Publish</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            @else
+            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+            @endif
+
         </div>
     </div>
 </div>
