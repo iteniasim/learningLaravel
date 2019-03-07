d88d7629f98ca9e69664d6a752c166a639b7db07
Asim Iteni <iteniasim.hulkstar@Gmail.com>
2019-03-07 13:30:52 +0545

    Filter Threads By Username

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,8 +37,18 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="/threads">All Threads</a>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                                aria-haspopup="true" aria-expanded="false">Browse</a>
+
+                            <div class="dropdown-menu">
+                                <a class="nav-link" href="/threads">All Threads</a>
+                                @if (auth()->check())
+                                <a class="nav-link" href="/threads?by={{ auth()->user()->name }}">My Threads</a>
+                                @endif
+                            </div>
+
                         </li>
 
                         <li class="nav-item">
