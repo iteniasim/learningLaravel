7b52e1cc913831519fe227b9f41acc150e28becd
Asim Iteni <iteniasim.hulkstar@Gmail.com>
2019-03-08 02:12:24 +0545

    Favorites Button UI work

--- app/Http/Controllers/FavouritesController.php
+++ app/Http/Controllers/FavouritesController.php
@@ -15,5 +15,7 @@ class FavouritesController extends Controller
     public function store(Reply $reply)
     {
         $reply->favourite();
+
+        return back();
     }
 }
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,7 +1,18 @@
 <div class="card">
     <div class="card-header">
-        <a href="#">{{ $reply->owner->name }}</a> said
-        {{ $reply->created_at->diffForHumans() }}...
+        <div class="d-flex justify-content-between">
+            <div>
+                <a href="#">{{ $reply->owner->name }}</a> said
+                {{ $reply->created_at->diffForHumans() }}...
+            </div>
+            <div>
+                <form method="POST" action="/replies/{{ $reply->id }}/favourites">
+                    @csrf
+                    <button type="Submit" class="btn btn-primary" {{ $reply->isFavourited() ? 'disabled' : '' }}>Favorite</button>
+                    {{ $reply->favourites()->count() }} {{ str_plural('favorite',$reply->favourites()->count()) }}
+                </form>
+            </div>
+        </div>
     </div>
 
     <div class="card-body">
